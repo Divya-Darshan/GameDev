200a51c8e4295cb40b6d7574f78eee9d3f90dc1f
Divya-Darshan <parukrish.darshan20@gmail.com>
2025-11-21 15:34:11 +0530

    new char with blend file

--- select/select.gd
+++ select/select.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var select_button = $HBoxContainer/TouchScreenButton
 
 var characters = [
+	preload("res://char scene/Steve/Steve.tscn"),
 	preload("res://char scene/nood/noob.tscn"),
 	preload("res://char scene/bird/bird.tscn"),
 	preload("res://char scene/doremon/doreamon.tscn"),
